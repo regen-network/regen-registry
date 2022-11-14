ae873e90d3e92db6361fed5a0a7e197b8aeb7536
Marie Gauthier <marie.gauthier63@gmail.com>
2022-11-14 09:01:17 +0100

    chore: update issue template and clean up repo (#1390)

--- src/lib/admin.ts
+++
@@ -1,7 +0,0 @@
-export default function isAdmin(user: any): boolean {
-  return (
-    user &&
-    user['https://regen-registry.com/roles'] &&
-    user['https://regen-registry.com/roles'].indexOf('admin') > -1
-  );
-}
